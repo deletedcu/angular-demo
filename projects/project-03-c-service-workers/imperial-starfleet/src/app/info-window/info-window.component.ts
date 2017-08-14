da2e497ef9951d27eee493e45258c4014fe39c86
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2017-08-14 08:19:35 -0600

    Adding service worker mini-project

---
+++ projects/project-03-c-service-workers/imperial-starfleet/src/app/info-window/info-window.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { ActivatedRoute }   from '@angular/router';
+
+@Component({
+  selector: 'app-info-window',
+  templateUrl: './info-window.component.html',
+  styleUrls: ['./info-window.component.css']
+})
+export class InfoWindowComponent implements OnInit {
+
+	dataBanks;
+
+  constructor(
+  	private http: Http,
+  	private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+  	this.route.params.forEach( param => {
+  		this.findTurret(param.id);
+  	});
+  }
+
+  findTurret(turretNumber){
+    this.http.get('http://localhost:3000/api/turret/'+turretNumber)
+    .subscribe(response => this.dataBanks = response.json());
+	}
+
+}
---
+++ projects/project-03-c-service-workers/solution-code/src/service-worker.js
@@ -0,0 +1,31 @@
+var CACHE_NAME = 'service-worker-cache-v1';
+var urlsToCache = [
+	'index.html',
+  'assets/images/tie-fighter.png',
+  'assets/images/turret.png'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
