01dfc5591d8c3bbed598eb5f62c8e348876ed83f
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2017-08-25 12:02:41 -0600

    Making internal defenses to match previous lessons module structure

--- projects/project-01-components/solution-code/src/app/internal-defenses/internal-defenses.module.ts
+++ projects/project-01-components/solution-code/src/app/internal-defenses/internal-defenses.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DeathSquareComponent } from './death-square.component';
+import { DeathSquareComponent } from './death-square/death-square.component';
 import { TurretComponent } from './turret/turret.component';
 import { LaserCannonComponent } from './laser-cannon/laser-cannon.component';
 
@@ -17,4 +17,4 @@ import { LaserCannonComponent } from './laser-cannon/laser-cannon.component';
     DeathSquareComponent
   ]
 })
-export class DeathSquareModule { }
+export class InternalDefensesModule { }
