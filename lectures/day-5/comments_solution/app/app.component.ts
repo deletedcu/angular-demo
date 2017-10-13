90a2906ea8ebc237ba3dd0a6b58e4281af6c98a4
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2017-10-12 17:03:26 -0700

    Comments app solution, cont'd

--- lectures/day-5/comments_solution/app/app.component.ts
+++ lectures/day-5/comments_solution/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { CommentService } from './comment/comment.service';
 
 @Component({
   selector: 'app-root',
@@ -7,29 +6,6 @@ import { CommentService } from './comment/comment.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  comments = [];
-  commentToEdit; 
-
-  constructor(private commentService: CommentService) {}
-
-  ngOnInit() {
-    this.comments = this.commentService.getComments();
-    this.commentService.onCommentsUpdated((myNewComments) => {
-      this.comments = myNewComments;
-    });
-  }
-
-  deleteComment(commentToDelete) {
-    this.commentService.deleteComment(commentToDelete);
-  }
-
-  makeCommentEditable(commentToEdit) {
-    this.commentToEdit = commentToEdit;
-  }
-
-  doneEditing() {
-    this.commentToEdit = null;
-  }
 }
 
 
--- lectures/day-5/comments_solution/app/app.module.ts
+++ lectures/day-5/comments_solution/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { CommentService } from './comment/comment.service';
 
 import { AppComponent } from './app.component';
+import { CommentsComponent } from './comments/comments.component';
 import { NewCommentComponent } from './new-comment/new-comment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CommentsComponent,
     NewCommentComponent
   ],
   imports: [
